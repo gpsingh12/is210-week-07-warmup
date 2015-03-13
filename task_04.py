4c0a1acd0abfcbcacbebc2d4e9493e6fbeb649e5
gpsingh12 <gurpreet_1441@yahoo.com>
2015-03-13 05:18:46 +0000

    process_data

---
+++ task_04.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Module uses a for loop to perform the
+   arithmetic functions.
+"""
+
+
+def process_data(data):
+    """Function takes one argument named data and
+       uses a for loop to get sum and average of
+       input data.
+       Arg:
+           total(int): sum of the input data.
+           average(float): average of input data.
+       Return:
+           returns a tuple providing sum and average of
+           input data.
+       Examples:
+                >>> process_data([1, 2, 3])
+                    (6, 2.0)
+    """
+    value = 0
+    for total in data:
+        value += total
+
+    average = float(value/(len(data)))
+    return value, average
