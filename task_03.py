cb3bb0a034808139ee46800ca032ab3094e009f1
gpsingh12 <gurpreet_1441@yahoo.com>
2015-03-13 00:39:06 +0000

    directions

---
+++ task_03.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Imports a module data and performs slicing
+   arithmetic operations on the tuples.
+"""
+import data
+DIRECTIONS = data.DIRECTIONS
+len(DIRECTIONS)
+DIRECTIONS = DIRECTIONS[0:3] + ('West',)
