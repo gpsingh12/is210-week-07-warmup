8b2348d1213d56903d8a17cfe7565943fa33cb9c
gpsingh12 <gurpreet_1441@yahoo.com>
2015-03-14 06:27:03 +0000

    edited

--- task_05.py
+++ task_05.py
@@ -3,15 +3,25 @@
 """Module uses for loop and proporties
    of lists to reverse elements of list.
 """
+
+
 def flip_keys(to_flip):
-    """
+    """Function takes one argument and returns
+       the reversed input nested, immutable list.
+       Args:
+           counter(int): the variable to increment the index
+                         while using for loop.
+            to_flip(mixed): the input immutable, nested list.
+        Returns:
+                returns the inner sequence items of list in
+                reverse order.
+        Examples:
+                >>> flip_keys([(1, 2, 3), 'hello'])
+                    [(3, 2, 1), 'hello']
+                    [(3, 2, 1), 'olleh']
     """
     counter = 0
     for item in to_flip:
-       #counter += 1
-        to_flip[counter] = item[::-1] #replace using index counter
+        to_flip[counter] = item[::-1] 
         counter += 1
-        print (to_flip)
-
-    
-    
+        print to_flip
